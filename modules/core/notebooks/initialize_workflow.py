5e8e4eaa5904a0877550bb462e7e3881422e16be
srijitcn <srijitcn>
2025-04-16 15:59:28 +0000

    creating model table

--- modules/core/notebooks/initialize_workflow.py
+++ modules/core/notebooks/initialize_workflow.py
@@ -1,4 +1,37 @@
 # Databricks notebook source
-from genesis_workbench.genesis_workbench.Workbench import Workbench
+#parameters to the notebook
+catalog = dbutils.widgets.text("catalog", "")
+schema = dbutils.widgets.text("schema", "")
 
-Workbench()
+
+# COMMAND ----------
+
+spark.sql(f"USE CATALOG {catalog}")
+
+spark.sql(f"CREATE SCHEMA IF NOT EXISTS {schema}")
+
+spark.sql(f"USE SCHEMA {schema}")
+
+# COMMAND ----------
+
+spark.sql("DROP TABLE IF EXISTS models")
+
+spark.sql(f"""
+CREATE TABLE models (
+    model_id BIGINT GENERATED ALWAYS AS IDENTITY,
+    model_name STRING,
+    model_display_name STRING,
+    model_source_version STRING,
+    model_origin STRING, --uc, huggingface, pypi, bionemo, etc
+    model_description_url STRING, --website to find more details about model
+    model_uc_name STRING,
+    model_uc_version STRING,
+    model_uc_added_by STRING,
+    model_uc_added_date TIMESTAMP,
+    is_model_deployed BOOLEAN,
+    model_deployed_date TIMESTAMP,
+    model_deployed_by STRING,
+    model_deploy_platform STRING, -- modelserving, dcs etc
+    model_invoke_url STRING
+)
+""")
