79aef08259534530a5d0dc4a9f9fc105fe7bad05
srijitcn <srijit.nair@databricks.com>
2025-04-21 16:39:34 -0400

    added common library wheel file to app

--- modules/core/deploy.sh
+++ modules/core/deploy.sh
@@ -10,6 +10,46 @@ fi
 ENV=$1
 EXTRA_PARAMS=${@: 2}
 
+echo "▶️ Building libraries"
+echo ""
+
+cd library/genesis_workbench
+poetry build
+
+if [ $? -ne 0 ]; then
+    echo "❗️ Error building wheel. Aborting deploy."
+    exit 1
+fi
+cd ../../
+
+echo "▶️ Copying library wheel file"
+echo ""
+
+cp library/genesis_workbench/dist/*.whl app/ 
+# Loop through all .whl files in the directory
+for file in library/genesis_workbench/dist/*.whl; do
+  echo "Checking $file"
+  # Check if the file exists (in case there are no .whl files)
+  if [ -f "$file" ]; then
+    # Extract just the filename (not the full path)
+    filename=$(basename "$file")
+    echo "Checking if $filename exists as dependency"
+    if ! grep -wq "$filename" app/requirements.txt; then        
+        echo "Adding $filename to the app dependency"
+        # Append the filename to the output file        
+        echo -e "\n$filename" >> app/requirements.txt
+    else
+        echo "Dependency already exists"
+    fi
+
+  fi
+done
+
+if [ $? -ne 0 ]; then
+    echo "❗️ Error copying wheel. Aborting deploy."
+    exit 1
+fi
+
 echo "▶️ Validating bundle"
 echo ""
 
@@ -40,6 +80,7 @@ fi
 
 echo "▶️ Deploying UI Application"
 echo ""
+
 databricks bundle run -t $ENV genesis_workbench_app $EXTRA_PARAMS
 if [ $? -ne 0 ]; then
     echo "❗️ Error. Aborting deploy."
