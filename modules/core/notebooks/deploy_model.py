ad9b3e9adec67d9f0ba1c7d9f5ff196f89b3e84a
srijitcn <srijit.nair@databricks.com>
2025-04-28 16:59:13 -0400

    wip adding workflow to deploy the model

--- modules/core/notebooks/deploy_model.py
+++ modules/core/notebooks/deploy_model.py
@@ -1,5 +1,13 @@
 # Databricks notebook source
 #parameters to the notebook
+
+dbutils.widgets.text("catalog", "genesis_workbench", "Catalog")
+dbutils.widgets.text("schema", "dev_srijit_nair_dbx_genesis_workbench_core", "Schema")
+dbutils.widgets.text("fq_model_uc_name", "catalog.schema.model_name", "UC Model Name")
+dbutils.widgets.text("model_version", "1", "UC Model version")
+dbutils.widgets.text("workload_type", "GPU_SMALL", "Endpoint Workload Type")
+dbutils.widgets.text("workload_size", "SMALL", "Endpoint Workload Size")
+
 catalog = dbutils.widgets.get("catalog")
 schema = dbutils.widgets.get("schema")
 fq_model_uc_name = dbutils.widgets.get("fq_model_uc_name")
