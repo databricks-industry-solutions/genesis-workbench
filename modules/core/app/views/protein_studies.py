3caaea1b6322ddb44285c188c3c64543152ac0fa
srijitcn <srijit.nair@databricks.com>
2025-06-03 09:48:58 -0400

    fixes to esmfold and started rfdiffusion

--- modules/core/app/views/protein_studies.py
+++ modules/core/app/views/protein_studies.py
@@ -1,5 +1,71 @@
 import streamlit as st
+from genesis_workbench.models import (ModelCategory, 
+                                      get_available_models, 
+                                      get_deployed_models,                                      
+                                      get_gwb_model_info,
+                                      deploy_model)
+
+from utils.streamlit_helper import (get_app_context, 
+                                    display_import_model_uc_dialog,
+                                    display_deploy_model_dialog)
+
+
 st.title(":material/biotech: Protein Structure Prediction")
+
+def display_protein_studies_settings(available_models_df,deployed_models_df):
+    p1,p2 = st.columns([2,1])
+
+    with p1:
+
+        st.markdown("###### Available Models:")
+        with st.form("deploy_model_form"):
+            col1, col2, = st.columns([1,1])    
+            with col1:
+                selected_model_for_deploy = st.selectbox("Model:",available_models_df["model_labels"],label_visibility="collapsed",)
+
+            with col2:
+                deploy_button = st.form_submit_button('Deploy')
+        if deploy_button:
+            display_deploy_model_dialog(selected_model_for_deploy)
+
+
+    if len(deployed_models_df) > 0:
+        with st.form("modify_deployed_model_form"):
+            col1,col2 = st.columns([2,1])
+            with col1:
+                st.markdown("###### Deployed Models")
+            with col2:
+                st.form_submit_button("Manage")
+            
+            st.dataframe(deployed_models_df, 
+                            use_container_width=True,
+                            hide_index=True,
+                            on_select="rerun",
+                            selection_mode="single-row")
+    else:
+        st.write("There are no deployed models")
+
+
+#load data for page
+with st.spinner("Loading data"):
+    if "available_protein_models_df" not in st.session_state:
+            available_protein_models_df = get_available_models(ModelCategory.PROTEIN_STUDIES, get_app_context())
+            available_protein_models_df["model_labels"] = (available_protein_models_df["model_id"].astype(str) + " - " 
+                                                + available_protein_models_df["model_display_name"].astype(str) + " [ " 
+                                                + available_protein_models_df["model_uc_name"].astype(str) + " v"
+                                                + available_protein_models_df["model_uc_version"].astype(str) + " ]"
+                                                )
+            st.session_state["available_protein_models_df"] = available_protein_models_df
+    available_protein_models_df = st.session_state["available_protein_models_df"]
+
+    if "deployed_protein_models_df" not in st.session_state:
+        deployed_protein_models_df = get_deployed_models(ModelCategory.PROTEIN_STUDIES, get_app_context())
+        deployed_protein_models_df.columns = ["Id", "Name", "Description", "Model Name", "Source Version", "UC Name/Version"]
+
+        st.session_state["deployed_protein_models_df"] = deployed_protein_models_df
+    deployed_protein_models_df = st.session_state["deployed_protein_models_df"]
 
 settings, esm, alpha = st.tabs(["Settings","Protein Design", "Protein Folding"])
 
+with settings:
+    display_protein_studies_settings(available_protein_models_df, deployed_protein_models_df)
---
+++ modules/rfdiffusion/notebooks/register_esmfold.py
@@ -0,0 +1,233 @@
+# Databricks notebook source
+# COMMAND ----------
+
+#%pip install databricks-sdk==0.50.0 databricks-sql-connector==4.0.2 torch==2.3.1 transformers==4.41.2 accelerate==0.31.0 mlflow==2.22.0
+#%pip install /Volumes/genesis_workbench/dev_srijit_nair_dbx_genesis_workbench_core/libraries/genesis_workbench-0.1.0-py3-none-any.whl --force-reinstall
+#dbutils.library.restartPython()
+
+# COMMAND ----------
+
+import sys
+
+sys.path.append("../src")
+
+
+# COMMAND ----------
+from esmfold.esmfold import ESMFoldPyFunc
+
+# COMMAND ----------
+
+dbutils.widgets.text("catalog", "genesis_workbench", "Catalog")
+dbutils.widgets.text("schema", "dev_srijit_nair_dbx_genesis_workbench_core", "Schema")
+dbutils.widgets.text("model_name", "esmfold", "Model Name")
+dbutils.widgets.text("experiment_name", "dbx_genesis_workbench_modules", "Experiment Name")
+dbutils.widgets.text("sql_warehouse_id", "w123", "SQL Warehouse Id")
+dbutils.widgets.text("user_email", "a@b.com", "User Id/Email")
+dbutils.widgets.text("cache_dir", "cache_dir", "Cache dir")
+
+CATALOG = dbutils.widgets.get("catalog")
+SCHEMA = dbutils.widgets.get("schema")
+MODEL_NAME = dbutils.widgets.get("model_name")
+EXPERIMENT_NAME = dbutils.widgets.get("experiment_name")
+USER_EMAIL = dbutils.widgets.get("user_email")
+SQL_WAREHOUSE_ID = dbutils.widgets.get("sql_warehouse_id")
+CACHE_DIR = dbutils.widgets.get("cache_dir")
+
+print(f"Cache dir: {CACHE_DIR}")
+cache_full_path = f"/Volumes/{CATALOG}/{SCHEMA}/{CACHE_DIR}"
+print(f"Cache full path: {cache_full_path}")
+# COMMAND ----------
+
+import os
+
+databricks_token = dbutils.notebook.entry_point.getDbutils().notebook().getContext().apiToken().getOrElse(None)
+os.environ["SQL_WAREHOUSE"]=SQL_WAREHOUSE_ID
+os.environ["IS_TOKEN_AUTH"]="Y"
+os.environ["DATABRICKS_TOKEN"]=databricks_token
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### ESMfold wrapped by mlflow Pyfunc model
+# MAGIC   - postprocessing of output to PDB string can be performed as part of the model
+# MAGIC   - useful for serving as users do not need to know about this
+# MAGIC     - useful to consider for other models if one wants to include other processing steps:
+# MAGIC       - e.g. additional relaxation of structures
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### Download model,tokenizer to the local disk of our compute
+
+# COMMAND ----------
+
+import mlflow
+import torch
+from transformers import AutoTokenizer, EsmForProteinFolding
+from transformers.models.esm.openfold_utils.protein import to_pdb, Protein as OFProtein
+from transformers.models.esm.openfold_utils.feats import atom14_to_atom37
+
+import transformers
+
+print(transformers.__version__)
+import accelerate
+
+print(accelerate.__version__)
+
+import mlflow
+from mlflow.models import infer_signature
+import os
+
+from typing import Any, Dict, List, Optional
+from genesis_workbench.models import (ModelCategory, 
+                                      import_model_from_uc,
+                                      get_latest_model_version)
+
+from genesis_workbench.workbench import AppContext
+
+# COMMAND ----------
+
+tokenizer = AutoTokenizer.from_pretrained("facebook/esmfold_v1", cache_dir=cache_full_path)
+model = EsmForProteinFolding.from_pretrained(
+    "facebook/esmfold_v1", low_cpu_mem_usage=True, cache_dir=cache_full_path
+)
+
+# COMMAND ----------
+
+esmfold_model = ESMFoldPyFunc()
+
+# COMMAND ----------
+
+test_input = [
+    "MADVQLQESGGGSVQAGGSLRLSCVASGVTSTRPCIGWFRQAPGKEREGVAVVNFRGDSTYITDSVKGRFTISRDEDSDTVYLQMNSLKPEDTATYYCAADVNRGGFCYIEDWYFSYWGQGTQVTVSSAAAHHHHHH"
+]
+from mlflow.types.schema import ColSpec, Schema
+
+signature = mlflow.models.signature.ModelSignature(
+    inputs=Schema([ColSpec(type="string")]),
+    outputs=Schema([ColSpec(type="string")]),
+    params=None,
+)
+
+# COMMAND ----------
+
+del model
+del tokenizer
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Register our model
+# MAGIC  - Pass the directory to our local HuggingFace Cache to the artifacts of the mlflow model
+# MAGIC    - this places the cache inside the logged model
+# MAGIC    - This cache is then used to build the model when model starts up (saves redownloading it)
+# MAGIC
+# MAGIC  - Tell mlflow we also needed some pip packages for the hosted server compute - this will install only these packages on the container for serving the model
+
+# COMMAND ----------
+
+mlflow.set_registry_uri("databricks-uc")
+
+
+
+with mlflow.start_run(run_name=f"register_{MODEL_NAME}"):
+    model_info = mlflow.pyfunc.log_model(
+        artifact_path="esmfold",
+        python_model=esmfold_model,
+        artifacts={
+            "cache": cache_full_path,
+        },
+        pip_requirements=[
+            "mlflow==2.15.1",
+            "cloudpickle==2.2.1",
+            "transformers>4.0",
+            "torch>2.0",
+            "torchvision",  # required for torch (note torch+torchvision without cuda v specified defaults to 12.4 as of Jan 2024)
+            "accelerate>0.31",
+        ],
+        input_example=test_input,
+        signature=signature,
+        registered_model_name=f"{CATALOG}.{SCHEMA}.{MODEL_NAME}",
+    )
+
+# COMMAND ----------
+
+model_uc_name=f"{CATALOG}.{SCHEMA}.{MODEL_NAME}"
+model_version = get_latest_model_version(model_uc_name)
+model_uri = f"models:/{model_uc_name}/{model_version}"
+
+app_context = AppContext(
+        core_catalog_name=CATALOG,
+        core_schema_name=SCHEMA
+    )
+
+import_model_from_uc(app_context,user_email=USER_EMAIL,
+                    model_category=ModelCategory.SINGLE_CELL,
+                    model_uc_name=f"{CATALOG}.{SCHEMA}.{MODEL_NAME}",
+                    model_uc_version=model_version,
+                    model_name="ESMFold2",
+                    model_display_name="ESMFold2",
+                    model_source_version="v2.0",
+                    model_description_url="https://github.com/facebookresearch/esm?tab=readme-ov-file#evolutionary-scale-modeling")
+
+# COMMAND ----------
+
+# from databricks.sdk import WorkspaceClient
+# from databricks.sdk.service.serving import (
+#     EndpointCoreConfigInput,
+#     ServedEntityInput,
+#     ServedModelInputWorkloadSize,
+#     ServedModelInputWorkloadType,
+#     AutoCaptureConfigInput,
+# )
+# from databricks.sdk import errors
+
+# w = WorkspaceClient()
+
+# endpoint_name = ENDPOINT_NAME
+
+# model_name = f"{CATALOG}.{SCHEMA}.{MODEL_NAME}"
+# versions = w.model_versions.list(model_name)
+# latest_version = max(versions, key=lambda v: v.version).version
+
+# print("version being served = ", latest_version)
+
+
+# served_entities = [
+#     ServedEntityInput(
+#         entity_name=model_name,
+#         entity_version=latest_version,
+#         name=MODEL_NAME,
+#         workload_type="GPU_SMALL",
+#         workload_size="Small",
+#         scale_to_zero_enabled=True,
+#     )
+# ]
+# auto_capture_config = AutoCaptureConfigInput(
+#     catalog_name=CATALOG,
+#     schema_name=SCHEMA,
+#     table_name_prefix=f"{MODEL_NAME}_serving",
+#     enabled=True,
+# )
+
+# try:
+#     # try to update the endpoint if already have one
+#     existing_endpoint = w.serving_endpoints.get(endpoint_name)
+#     # may take some time to actually do the update
+#     status = w.serving_endpoints.update_config(
+#         name=endpoint_name,
+#         served_entities=served_entities,
+#         auto_capture_config=auto_capture_config,
+#     )
+# except errors.platform.ResourceDoesNotExist as e:
+#     # if no endpoint yet, make it, wait for it to spin up, and put model on endpoint
+#     status = w.serving_endpoints.create_and_wait(
+#         name=endpoint_name,
+#         config=EndpointCoreConfigInput(
+#             name=endpoint_name,
+#             served_entities=served_entities,
+#             auto_capture_config=auto_capture_config,
+#         ),
+#     )
+
+# print(status)
