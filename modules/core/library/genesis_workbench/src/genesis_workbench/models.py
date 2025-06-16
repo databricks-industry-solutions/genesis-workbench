354bc60ae18f8447b9a66ae792607c2e2869bb1b
srijitcn <srijit.nair@databricks.com>
2025-06-16 14:28:05 -0400

    bug fixes and refactor

--- modules/core/library/genesis_workbench/src/genesis_workbench/models.py
+++ modules/core/library/genesis_workbench/src/genesis_workbench/models.py
@@ -100,6 +100,8 @@ def get_available_models(model_category : ModelCategory,app_context:AppContext) 
             WHERE \
                 model_category = '{str(model_category)}' AND is_active=true \
             ORDER BY model_id DESC "
+                model_category = '{str(model_category)}' AND is_active=true \
+            ORDER BY model_id DESC "
     
     print(query)
     result_df = execute_select_query(query)
@@ -116,9 +118,7 @@ def get_deployed_models(model_category : ModelCategory, app_context:AppContext)-
                 models.model_id = model_deployments.model_id \
             WHERE \
                 model_category = '{str(model_category)}' and model_deployments.is_active=true \
-            ORDER BY deployment_id DESC "
-    
-    print(query)
+            ORDER BY model_id DESC "
     
     result_df = execute_select_query(query)
     return result_df
