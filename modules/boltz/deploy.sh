b725afd1c55d729154e15d4659511718a5ed533a
srijitcn <srijitcn>
2025-06-20 14:26:55 +0000

    added boltz model

---
+++ modules/boltz/deploy.sh
@@ -0,0 +1,31 @@
+
+#!/bin/bash
+set -e
+
+if [ "$#" -lt 1 ]; then
+    echo "Usage: $0 <env> <additional build variables>"
+    echo 'Example: deploy dev --var="dev_user_prefix=scn,core_catalog_name=genesis_workbench,core_schema_name=dev_srijit_nair_dbx_genesis_workbench_core"'
+    exit 1
+fi
+
+ENV=$1
+EXTRA_PARAMS=${@: 2}
+
+echo ""
+echo "▶️ Validating bundle"
+echo ""
+
+databricks bundle validate $EXTRA_PARAMS
+
+echo ""
+echo "▶️ Deploying bundle"
+echo ""
+
+databricks bundle deploy -t $ENV $EXTRA_PARAMS
+
+echo ""
+echo "▶️ Running model registration job"
+echo ""
+
+databricks bundle run -t $ENV register_proteinmpnn $EXTRA_PARAMS
+
