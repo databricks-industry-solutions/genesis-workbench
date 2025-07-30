67319ccd2dbaa97b8f4ce14af8c463ac4faf02c5
srijitcn <srijitcn>
2025-07-30 16:56:40 +0000

    file download url fixes

--- modules/protein_studies/alphafold/alphafold_v2.3.2/notebooks/download_pdb_mmcif.py
+++ modules/protein_studies/alphafold/alphafold_v2.3.2/notebooks/download_pdb_mmcif.py
@@ -6,6 +6,8 @@
 # MAGIC %sh
 # MAGIC if [ ! -d "$MODEL_VOLUME/datasets/pdb_mmcif" ]; then
 # MAGIC     echo "Downloading pdb_mmcif"
+# MAGIC     NEWLINE='aria2c "https://files.wwpdb.org/pub/pdb/data/status/obsolete.dat" --dir="${ROOT_DIR}"'
+# MAGIC     sed -i '$c\'"$NEWLINE" download_pdb_mmcif.sh
 # MAGIC     cd /app/alphafold/scripts
 # MAGIC     ./download_pdb_mmcif.sh /local_disk0/downloads
 # MAGIC     cd /
